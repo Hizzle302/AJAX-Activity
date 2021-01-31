bbd8f1de5cc92ddba759b202bfb23e8026b6ec93
Hizzle 302 <azcrawley@yahoo.com>
2021-01-30 23:47:40 -0500

    done

--- ajax.js
+++ ajax.js
@@ -1,9 +1,27 @@
+// #1 Create an XMLHTTP Request Object
+
 var dogs = new XMLHttpRequest();
+
+// #2 Create a call back function
+
 dogs.onreadystatechange = function() {
     if(dogs.readyState === 4) {
-     documentgetElementById('ajax').innerHtml = dogs.responseText;   
+    var pic = JSON.parse(request.responseText);
+     documentgetElementById('pic1').src = pic.message[0];
+     documentgetElementById('pic2').src = pic.message[1];
+     documentgetElementById('pic3').src = pic.message[2];   
     }
 
-}
-dogs.open('GET', 'https://dog.ceo/api/breeds/image/random')
+};
+// Calling Button Function
+
+function sendAJAX() {
+
+// #3 Open a request using GET 
+
+dogs.open('GET', 'https://dog.ceo/api/breeds/image/random');
+
+// #4 Send request
+
 dogs.send();
+}
