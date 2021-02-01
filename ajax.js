ce7263dc9f1fcd2ae7f82137dfc6b33e5ace1282
Hizzle 302 <azcrawley@yahoo.com>
2021-01-31 19:20:24 -0500

    fixed errors works now

--- ajax.js
+++ ajax.js
@@ -1,27 +1,14 @@
-// #1 Create an XMLHTTP Request Object
-
 var dogs = new XMLHttpRequest();
-
 // #2 Create a call back function
-
-dogs.onreadystatechange = function() {
-    if(dogs.readyState === 4) {
-    var pic = JSON.parse(request.responseText);
-     documentgetElementById('pic1').src = pic.message[0];
-     documentgetElementById('pic2').src = pic.message[1];
-     documentgetElementById('pic3').src = pic.message[2];   
-    }
-
-};
-// Calling Button Function
-
 function sendAJAX() {
-
-// #3 Open a request using GET 
-
-dogs.open('GET', 'https://dog.ceo/api/breeds/image/random');
-
-// #4 Send request
-
-dogs.send();
+  dogs.onreadystatechange = function () {
+    if (dogs.readyState === 4) {
+      var image = JSON.parse(dogs.responseText);
+      document.getElementById("pic1").src = image.message[0];
+      document.getElementById("pic2").src = image.message[1];
+      document.getElementById("pic3").src = image.message[2];
+    }
+  };
+  dogs.open("GET", "https://dog.ceo/api/breeds/image/random/3");
+  dogs.send();
 }
